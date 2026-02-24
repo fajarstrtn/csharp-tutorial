d5fb0b373f9b7112b83dd635b0ea43ddde4a1970
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-02-24 09:28:11 +0700

    feat: add Introduction.cs file

---
+++ beginner/introduction/Introduction.cs
@@ -0,0 +1,55 @@
+using System;
+
+/*
+ * C# is a modern, object-oriented programming language created by Microsoft in 2000,
+ * as part of the .NET framework. It runs on the .NET platform.
+ * It's mainly used to build:
+ * 1. Windows applications
+ * 2. Web applications
+ * 3. Games (especially with Unity)
+ * 4. APIs and backend services
+ * 5. Desktop software
+ *
+ * C# does not run directly on the computer. It goes through steps:
+ * 1. You write C# code (.cs file).
+ * 2. It gets compiled into Intermediate Language (IL).
+ * 3. The .NET runtime converts it into machine code.
+ * 4. The program runs
+ *
+ * Ilustration:
+ * C# Code → Compiler → IL Code → .NET Runtime → Machine Code → Run
+ *
+ * C# works closely with the .NET platform,
+ * which provides the runtime, libraries and tools for application development.
+ * Over time, several frameworks and technologies have evolved
+ * around C# to support diverse platforms:
+ * 1. .NET Framework
+ * The original Windows-only platform for building desktop and web applications.
+ * Commonly used for WPF, WinForms and ASP.NET applications.
+ * 2. .NET Core
+ * Cross-platform, open-source version of .NET.
+ * Used to build apps that run on Windows, Linux and macOS.
+ * Supports web APIs, microservices and command-line tools.
+ * 3. .NET 5 and Later (.NET Unified Platform)
+ * Unifies .NET Framework and .NET Core into a single platform.
+ * Provides performance improvements and cross-platform support
+ * for all application types.
+ * 4. ASP.NET & ASP.NET Core
+ * Frameworks for developing dynamic web applications,
+ * RESTful APIs and MVC-based web services.
+ * 5. Entity Framework Core (EF Core)
+ * Object-Relational Mapper (ORM) for database operations.
+ * Allows developers to interact with databases
+ * using C# objects instead of SQL queries.
+ * 6. Unity Engine
+ * A game development platform that uses C# scripts for game logic.
+ *
+ * C# is yet powerful and secure, where one of its features is type-safe
+ * and managed by the .NET runtime. */
+public class Introduction
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World"); // Output: Hello World
+    }
+}
