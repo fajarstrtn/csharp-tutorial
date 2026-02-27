2ab054043fb2800bbe7e5ff7123616aea9fd08e1
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-02-27 11:21:42 +0700

    feat: add new method sayHi() and other explanations in Comment.cs file

--- beginner/comment/Comment.cs
+++ beginner/comment/Comment.cs
@@ -2,7 +2,23 @@ using System;
 
 /*
  * Comments can be used to explain C# code, and to make it more readable.
- * It can also be used to prevent execution when testing alternative code. */
+ * It can also be used to prevent execution when testing alternative code.
+ *
+ * Comments are an important part of the code.
+ * They allow developers to explain their code,
+ * document important information and make the code easier to understand.
+ * Whatever you write in comments, compilers ignore it and do not execute them.
+ *
+ * For writing good and productive comments please remember the points mentioned below:
+ * 1. Comments are self-explanatory       : Comments should add value
+ * by explaining complex or non-obvious logic.
+ * 2. Comments should be clear and concise: Avoid writing overly complex comments
+ * that could confuse the reader.
+ *
+ * In C#, There are three types of comments which are defined below:
+ * 1. Single-line comments
+ * 2. Multi-line Comments
+ * 3. XML comments */
 public class Comments
 {
     public static void Main(string[] args)
@@ -16,5 +32,15 @@ public class Comments
          * This is a multi-line comment.
          * Print "Have a nice day" */
         Console.WriteLine("Have a nice day");   // Output: Have a nice day
+    }
+
+    /// <summary>
+    /// Generate a message with specific username.
+    /// </summary>
+    /// <param name="username">The unique username.</param>
+    /// <returns>No return message, just print it to the console.</returns>
+    public static void sayHi(string username)
+    {
+        Console.WriteLine("Hello, " + username);
     }
 }
