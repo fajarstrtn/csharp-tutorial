f107751b42ad17a20d75c3db45e3a7609951f1a8
Fajar Satriatna <fajarsatriatna24@gmail.com>
2026-02-27 11:12:47 +0700

    fix: update TypeCasting.cs file

--- beginner/type_casting/TypeCasting.cs
+++ beginner/type_casting/TypeCasting.cs
@@ -28,5 +28,38 @@ public class TypeCasting
         Console.WriteLine(str);      // Output: 10,5
         Console.WriteLine(@bool);    // Output: True
         Console.WriteLine(_decimal); // Output: 10000000000
+
+        byte number = 65;
+
+        /*
+         * In C#, numeric types support implicit conversions among themselves, but not to char or bool.
+         * Also, char and bool are incompatible.
+         * The compiler ensures type compatibility before conversion. */
+        short @short = number;
+
+        /*
+         * Converting a short (16-bit signed integer) to a char (16-bit Unicode character)
+         * requires an explicit cast because, while both are 16-bit,
+         * a short can hold negative values that do not map to valid Unicode characters.
+         * Since char is unsigned (range 0 to 65.535) and short is signed
+         * (range this conversion is technically a narrowing conversion, not a widening one. 
+         * Converting short to char char @char = @short; */
+        int @int = number;
+        long @long = @int;
+        float @float = @long;
+        double @double = @float;
+        Console.WriteLine(@double); // Output: 65
+
+        /*
+         * Explicit type casting is the process of manually converting one data type
+         * into another using a cast operator (type).
+         * It is required when converting from a larger data type to a smaller one,
+         * or between incompatible types and may cause data loss or exceptions. */
+        double num4 = 1024.668;
+        int num5 = (int)num4;
+        Console.WriteLine(num5); // Output: 1024
+
+        char ch1 = Convert.ToChar(65);
+        Console.WriteLine(ch1); // Output: A
     }
 }
